c6dbfdb186281c28b5fc2ce504b5cc9a53be808d
Mohammed.Jafar2 <mohammed.jafar2@target.com>
2017-06-16 16:14:11 +0530

    added basic code

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo "hi";
+echo '{
+"color": "green",
+"card": {
+"style": "link",
+"url": "http://i0.kym-cdn.com/photos/images/newsfeed/000/131/786/tumblr_ljkeuyjp1a1qafrh6.gif",
+"id": null,
+"title": "Timely card layout",
+"description": "  has submitted his timesheet",
+"date": 1497609802,
+"thumbnail": {
+"url": "https://c.martech.zone/wp-content/uploads/2010/06/example-logo.png",
+"url@2x": "https://c.martech.zone/wp-content/uploads/2010/06/example-logo.png",
+"width": 1193,
+"height": 564
+}
+},
+"message": "test",
+"message_format": "html"
+}';
 ?>
