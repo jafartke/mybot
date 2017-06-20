987e15944163a10a453d47ab823c9cedcdda4ef8
Mohammed.Jafar2 <mohammed.jafar2@target.com>
2017-06-20 15:19:17 +0530

    added

---
+++ test.php
@@ -0,0 +1,227 @@
+<?php
+
+$test = '
+
+Array
+(
+    [200] => Array
+        (
+            [slotLabel] => SLOT 1
+            [allowedComponents] => Array
+                (
+                    [0] => 13603
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12003
+                    [1] => 13206
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [class] => tpc-wrap _x0 _y0 _w12 _h1
+        )
+    [400] => Array
+        (
+            [slotLabel] => SLOT 2
+            [allowedComponents] => Array
+                (
+                    [0] => 13801
+                    [1] => 13403
+                    [2] => 13501
+                    [3] => 14001
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12003
+                    [1] => 13204
+                    [2] => 13802
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [slotLayouts] => Array
+                (
+                    [0] => sl_web_a_0001_v01
+                )
+            [class] => tpc-wrap _x0 _y1 _w12 _h1
+        )
+    [600] => Array
+        (
+            [slotLabel] => DVM
+            [allowedComponents] => Array
+                (
+                )
+            [archivedComponents] => Array
+                (
+                )
+            [adminComponents] => Array
+                (
+                )
+            [class] => tpc-wrap _x0 _y2 _w12 _h1 _reserved
+            [componentPath] => dvm
+        )
+    [800] => Array
+        (
+            [slotLabel] => SLOT 3
+            [allowedComponents] => Array
+                (
+                    [0] => 13603
+                    [1] => 13906
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12003
+                    [1] => 13206
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [class] => tpc-wrap _x0 _y3 _w12 _h1
+        )
+    [1000] => Array
+        (
+            [slotLabel] => SLOT 4
+            [allowedComponents] => Array
+                (
+                    [0] => 12502
+                    [1] => 13503
+                    [2] => 13801
+                    [3] => 13403
+                    [4] => 13501
+                    [5] => 13906
+                    [6] => 14001
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12003
+                    [1] => 12803
+                    [2] => 13204
+                    [3] => 13802
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [slotLayouts] => Array
+                (
+                    [0] => sl_web_a_0001_v01
+                    [1] => sl_web_a_0002_v01
+                )
+            [class] => tpc-wrap _x0 _y4 _w12 _h1
+        )
+    [1100] => Array
+        (
+            [slotLabel] => SLOT 5
+            [allowedComponents] => Array
+                (
+                    [0] => 13501
+                    [1] => 13906
+                    [2] => 14001
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12502
+                    [1] => 13802
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [class] => tpc-wrap _x0 _y5 _w12 _h1
+        )
+    [1200] => Array
+        (
+            [slotLabel] => SLOT 6
+            [allowedComponents] => Array
+                (
+                    [0] => 13906
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12101
+                    [1] => 12003
+                    [2] => 13215
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [class] => tpc-wrap _x0 _y6 _w12 _h1
+        )
+    [1400] => Array
+        (
+            [slotLabel] => SLOT 7
+            [allowedComponents] => Array
+                (
+                    [0] => 13905
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12103
+                    [1] => 12003
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [class] => tpc-wrap _x0 _y7 _w12 _h1
+        )
+    [1500] => Array
+        (
+            [slotLabel] => DVM 2
+            [allowedComponents] => Array
+                (
+                )
+            [archivedComponents] => Array
+                (
+                )
+            [adminComponents] => Array
+                (
+                )
+            [class] => tpc-wrap _x0 _y2 _w12 _h1 _reserved
+            [componentPath] => dvm2
+        )
+    [1600] => Array
+        (
+            [slotLabel] => SLOT 8
+            [allowedComponents] => Array
+                (
+                    [0] => 13901
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12102
+                    [1] => 12003
+                    [2] => 13216
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [class] => tpc-wrap _x0 _y8 _w12 _h1
+        )
+    [1800] => Array
+        (
+            [slotLabel] => SLOT 9
+            [allowedComponents] => Array
+                (
+                    [0] => 13901
+                )
+            [archivedComponents] => Array
+                (
+                    [0] => 12003
+                    [1] => 13216
+                )
+            [adminComponents] => Array
+                (
+                    [0] => 13604
+                )
+            [class] => tpc-wrap _x0 _y9 _w12 _h1
+        )
+)
+';
+?>
