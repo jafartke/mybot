99b259885446284bc4c10efcf42ce1ad7c73e682
Mohammed.Jafar2 <mohammed.jafar2@target.com>
2017-08-02 15:07:58 +0530

    # Please include a useful commit message!

--- index.php
+++ index.php
@@ -3,19 +3,19 @@ $postdata = json_decode(file_get_contents("php://input"), true);
 $action = '';
 $message = $postdata['item']['message']['message'];
 $name = explode(" ", $message);
-//$url = "http://api.giphy.com/v1/gifs/search?q=".$name[1]."&api_key=dc6zaTOxFJmzC";
-$url = "http://api.giphy.com/v1/gifs/search?q=friday&api_key=dc6zaTOxFJmzC";
+$url = "http://api.giphy.com/v1/gifs/search?q=".$name[1]."&api_key=dc6zaTOxFJmzC";
+
+
+//$url = "http://api.giphy.com/v1/gifs/search?q=friday&api_key=dc6zaTOxFJmzC";
 $json     = file_get_contents($url);
 $obj = json_decode($json);
 $minCount =  0;
 $maxCount =  count($obj->data);
 $gifURL =  $obj->data[rand($minCount,$maxCount)]->images->original_still->url;
-
 $response = array(
   "color" => "red",
-  "message" => "testing",
+  "message" => "<img src='".$gifURL."'> testing",
   "message_format" => "html"
 );
-return $response;
-
+return json_encode($response, JSON_FORCE_OBJECT);
 ?>
