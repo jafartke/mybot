9a7c973741a97ae7e93fd5816b14505154f3d225
Mohammed.Jafar2 <mohammed.jafar2@target.com>
2017-08-02 12:09:59 +0530

    # Please include a useful commit message!

--- index.php
+++ index.php
@@ -1,39 +1,25 @@
-<script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
-
-<script>
-$(document).ready(function(){
-    var finalURL = "";
-    $.getJSON('https://api.giphy.com/v1/gifs/search?q=friday&api_key=dc6zaTOxFJmzC', function(data) {
-    $.each(data.data,function(i,obj){
-    					finalURL = obj.embed_url;
-    });
-    console.log(finalURL)
-    });
-});
-</script>
 <?php
-//echo $imageURL = '<script>document.write(finalURL);</script>';
-//exit;
 $postdata = json_decode(file_get_contents("php://input"), true);
 $action = '';
 $message = $postdata['item']['message']['message'];
 $name = explode(" ", $message);
-//$url = "http://api.giphy.com/v1/gifs/search?q=".$name[1]."&api_key=dc6zaTOxFJmzC";
-//$json = file_get_contents($url);
-//$obj = json_decode($json);
-//$image = $obj->data->images->fixed_height->url;
+$url = "http://api.giphy.com/v1/gifs/search?q=".$name[1]."&api_key=dc6zaTOxFJmzC";
+//$url = "http://api.giphy.com/v1/gifs/search?q=friday&api_key=dc6zaTOxFJmzC";
+$json = file_get_contents($url);
+$obj = json_decode($json);
+echo "<pre>";
+$minCount =  0;
+$maxCount =  count($obj->data);
+$gifURL =  $obj->data[rand($minCount,$maxCount)]->images->original_still->url;
 
-echo '{
+$return =  '{
 "color": "green",
 "card": {
 "style": "link",
 "url": "http://i0.kym-cdn.com/photos/images/newsfeed/000/131/786/tumblr_ljkeuyjp1a1qafrh6.gif",
 "id": null,
-"title": "Timely card layout",
-"description": "  has submitted his timesheet",
+"title": "GIFY",
+"description": "$gifURL",
 "date": 1497609802,
 "thumbnail": {
 "url": "https://c.martech.zone/wp-content/uploads/2010/06/example-logo.png",
@@ -42,7 +28,8 @@ echo '{
 "height": 564
 }
 },
-"message": "What do you want.. <img src="https://giphy.com/embed/3o84U0cCBPv6F0IIZa">'.$image.'?",
+"message": "What do you want.. <img src="$gifURL">'.$image.'?",
 "message_format": "html"
 }';
+return $return;
 ?>
